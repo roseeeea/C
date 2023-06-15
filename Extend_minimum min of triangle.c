0ac123518cb02065c6353720619bce59f1d52084
roselian <99393424+roseeeea@users.noreply.github.com>
2023-06-15 09:32:54 +0800

    Update and rename Extend_minimum min of triangle to Extend_minimum min of triangle.c

--- Extend_minimum min of triangle.c
+++ Extend_minimum min of triangle.c
@@ -1,3 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+int xy[(int) 1e6];
+
+int main(void){
+	int t;
+    
+    scanf("%d", &t);
+    while(t--){
+        long long ans=0; //ans position
+    	int a, b, c, d;
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        memset(xy, 0, sizeof(xy)); //memset
+        for(int i=a; i<=b; i++){
+        	xy[i+b]++;
+            xy[i+c+1]--; //i+c+1
+        }
+        for(int i=1; i<=b+c; i++){
+        	xy[i]=xy[i]+xy[i-1]; //xy[i-1]
+        }
+        for(int i=c; i<=b+c; i++){
+        	long long int ub=(i/1.5)+1; //long long
+            ans=ans+xy[i]*fmax((fmin(d, i-1)-fmax(c, ub)+1), 0); //xy[i], *2fmax
+        }
+        
+        printf("%lld\n", ans);
+        }
+}
+//----------------------------------------------------------
 //https://acm.cs.nthu.edu.tw/problem/12494/
 #include<stdio.h>
 
